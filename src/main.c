81a75cf858dc9f84d85d0aa930434c1a36fdc675
Parth Patel <parthp.tech@gmail.com>
2026-06-23 23:04:26 +0530

    refactor(shell): comment out debug print statements for tokenization

--- src/main.c
+++ src/main.c
@@ -125,9 +125,9 @@ int main() {
             }
         }
 
-           for (int i=0;i<5;i++) {
-            printf("Token %d: %s\n", i, argv[i]);
-        }
+        //    for (int i=0;i<5;i++) {
+        //     printf("Token %d: %s\n", i, argv[i]);
+        // }
 
 
         // Handling builtin commands
